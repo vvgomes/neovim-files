b80d0205438c51f2bfdc0960bb2c57cacc083674
vvgomes <vinicius.vgomes@nubank.com.br>
2025-10-26 23:19:52 -0400

    remap of <C-o> due to conflict with Zellij

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- gb to navigate back from gd
+-- gB to navigate forward
+vim.keymap.set("n", "gb", "<C-o>")
+vim.keymap.set("n", "gB", "<C-i>")
