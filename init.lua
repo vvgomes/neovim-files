437d70c87eeeecdf91a1ffe8eb6fac1105b45e52
vvgomes <vinicius.vgomes@nubank.com.br>
2025-11-27 19:45:36 -0500

    No spell / wrap; exec in Nushell

--- init.lua
+++ init.lua
@@ -3,3 +3,12 @@ require("config.lazy")
 
 -- fixes clipboard for Nushell
 vim.opt.clipboard = "unnamedplus"
+
+-- no line wrap
+vim.opt.wrap = false
+
+-- no spell check, LazyVim enables it for markdown by default
+vim.opt.spell = false
+
+-- exec external commands with Nushell
+vim.opt.shell = "/opt/homebrew/bin/nushell"
